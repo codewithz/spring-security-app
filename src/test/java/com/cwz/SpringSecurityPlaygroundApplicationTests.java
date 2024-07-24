71a8a0090a3bf4945b3f6cc0832923810d1f6d60
Zartab Nakhwa <zartab@codewithz.com>
2024-07-24 21:21:44 +0530

    Added controllers and credentials are in app.properties file

---
+++ src/test/java/com/cwz/SpringSecurityPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
