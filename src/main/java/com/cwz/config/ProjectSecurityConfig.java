dcec9e55b18ff4a9a81b0ed7fde2923aba915664
Zartab Nakhwa <zartab@codewithz.com>
2024-07-24 22:30:55 +0530

    Added JdbcUserDetailsManager

--- src/main/java/com/cwz/config/ProjectSecurityConfig.java
+++ src/main/java/com/cwz/config/ProjectSecurityConfig.java
@@ -13,8 +13,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -36,17 +39,20 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user= User.withUsername("user").password("{noop}password")
+//                .authorities("READ").build();
+//
+//        UserDetails admin= User.withUsername("admin").password("{bcrypt}$2a$12$PNcmiGuQfg38Puw79fDiXeSadDMzghhtxgDwZXRQkEoxz4L7VMuFC")
+//                .authorities("WRITE","READ").build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user= User.withUsername("user").password("{noop}password")
-                .authorities("READ").build();
-
-        UserDetails admin= User.withUsername("admin").password("{bcrypt}$2a$12$PNcmiGuQfg38Puw79fDiXeSadDMzghhtxgDwZXRQkEoxz4L7VMuFC")
-                .authorities("WRITE","READ").build();
-
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
 //        return new BCryptPasswordEncoder();
