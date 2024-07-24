154d254dac296cbb5890cb01161a5746a4f82618
Zartab Nakhwa <zartab@codewithz.com>
2024-07-25 00:07:00 +0530

    AuthN & AuthZ : Added Authority and updated Customer.java

--- src/main/java/com/cwz/model/Customer.java
+++ src/main/java/com/cwz/model/Customer.java
@@ -1,24 +1,94 @@
 package com.cwz.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+
+
+import java.util.Set;
 
 @Entity
 @Table(name = "customer")
-@Getter @Setter
 public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="customer_id")
     private long id;
 
     private String email;
 
     private String pwd;
 
+    @Column(name="mobile_number")
+    private String mobileNumber;
+
     @Column(name="role")
     private String role;
 
+    @Column(name="create_dt")
+    private String createDt;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "customer",fetch = FetchType.EAGER)
+    private Set<Authority> authorities;
+
+    public Customer() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getCreateDt() {
+        return createDt;
+    }
+
+    public void setCreateDt(String createDt) {
+        this.createDt = createDt;
+    }
+
+    public Set<Authority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Authority> authorities) {
+        this.authorities = authorities;
+    }
 }
